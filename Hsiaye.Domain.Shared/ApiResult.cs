7afda9266cc7c76503fcfbb767a138e82bb60d4d
sunzhimin <891424065@qq.com>
2021-02-10 21:02:42 +0800

    api统计返回数据格式、友好提示异常类

---
+++ Hsiaye.Domain.Shared/ApiResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hsiaye.Domain.Shared
+{
+    public class ApiResult
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
---
+++ Hsiaye.Domain.Shared/UserFriendlyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hsiaye.Domain.Shared
+{
+    public class UserFriendlyException : Exception
+    {
+        public UserFriendlyException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+        public UserFriendlyException(string message) : base(message) { }
+        public int Code { get; set; }
+    }
+}
