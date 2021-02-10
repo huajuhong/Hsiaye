bff8b73e37676a8511a742d7623c09e088fa6e42
sunzhimin <891424065@qq.com>
2021-02-10 21:02:57 +0800

    demo 优化

--- Hsiaye.Web/Controllers/DemoController.cs
+++ Hsiaye.Web/Controllers/DemoController.cs
@@ -1,6 +1,10 @@
-﻿using Hsiaye.Dapper;
+﻿using Hsiaye.Application.Authorization;
+using Hsiaye.Dapper;
 using Hsiaye.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +16,14 @@ namespace Hsiaye.Web.Controllers
     public class DemoController : ControllerBase
     {
         private readonly IDatabase _database;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public DemoController(IDatabase database)
+        public DemoController(IDatabase database, IHttpContextAccessor httpContextAccessor)
         {
             _database = database;
+            _httpContextAccessor = httpContextAccessor;
         }
+
 
         //http://localhost:55448/demo/list?page=0&limit=3
         public IEnumerable<Demo> List(int page, int limit)
@@ -24,6 +31,11 @@ namespace Hsiaye.Web.Controllers
             ISort sort = Predicates.Sort<Demo>(x => x.Id, false);
             var list = _database.GetPage<Demo>(null, new List<ISort> { sort }, page, limit);
             return list;
+        }
+        [Authorize]
+        public string Current()
+        {
+            return "1";
         }
     }
 }
