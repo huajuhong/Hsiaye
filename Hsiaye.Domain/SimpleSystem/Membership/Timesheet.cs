2409e45b68745ca50c9c4374401e10af6b221326
sunzhimin <891424065@qq.com>
2021-05-05 22:03:27 +0800

    工时项目/工时表

---
+++ Hsiaye.Domain/SimpleSystem/Membership/Timesheet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hsiaye.Domain
+{
+    //工时表
+    public class Timesheet
+    {
+        public int Id { get; set; }
+        public int CreateMemberId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int ProjectId { get; set; }
+        public int MembershipId { get; set; }
+        public DateTime Date { get; set; }//日期，精确到天
+        public TimeSpan Duration { get; set; }//时长，精确到分
+        public bool IsOvertime { get; set; }//是否为加班
+        public string Description { get; set; }//说明
+    }
+}
---
+++ Hsiaye.Domain/SimpleSystem/Membership/TimesheetProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hsiaye.Domain
+{
+    //工时表项目
+    public class TimesheetProject
+    {
+        public int Id { get; set; }
+        public int CreateMemberId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int OrganizationUnitId { get; set; }
+        public string Name { get; set; }
+    }
+}
