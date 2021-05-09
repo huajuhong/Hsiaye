18ee20122c95422ff94526b700bf4d7274478b66
sunzhimin <891424065@qq.com>
2021-05-09 17:41:12 +0800

    todo完善

--- Hsiaye.Domain/Todo/Todo.cs
+++ Hsiaye.Domain/Todo/Todo.cs
@@ -13,16 +13,16 @@ namespace Hsiaye.Domain
         public int CreateMemberId { get; set; }
         public string Title { get; set; }//标题
         public string Tag { get; set; }//标签
-        public DateTime 到期时间 { get; set; }
-        public string 分配到 { get; set; }
-        public string 分配自 { get; set; }
+        public DateTime ExpireTime { get; set; }//到期时间
+        public int DistributeToMemberId { get; set; }//分配到
+        public int DistributeFormMemberId { get; set; }//分配自
         public int ParentId { get; set; }
-        public TimeSpan 估算时间 { get; set; }
+        public TimeSpan EstimatedTime { get; set; }//估算时间
         public DateTime StartTime { get; set; }//开始时间
         public string Category { get; set; }
         public DateTime LastModifyTime { get; set; }//上次修改时间
         public int LastModifyMemberId { get; set; }//上次修改者
-        public TimeSpan TimeRemaining { get; set; }//剩余时间（作为列显示），值为：到期时间-开始时间
+        public TimeSpan TimeRemaining { get; set; }//剩余时间（作为计算列显示），值为：到期时间-开始时间
         public int DependTodoId { get; set; }//这个任务必须等该 依赖性 任务完成才可进行
         public TimeSpan SpendTime { get; set; }//所花费时间
         public bool Remind { get; set; }//提醒
@@ -40,8 +40,8 @@ namespace Hsiaye.Domain
 
         public DateTime NoteTime { get; set; }//注释时间 
         public string NoteContent { get; set; }//注释内容
-        public int 状态 { get; set; }
-        public int 子任务完成度 { get; set; }
+        public TodoState State { get; set; }
+        public int SubtaskCompletePercent { get; set; }//子任务完成百分比
     }
     public enum TodoRepeat
     {
@@ -50,5 +50,10 @@ namespace Hsiaye.Domain
         每周 = 2,
         每月 = 3,
         每年 = 4,
+    }
+    public enum TodoState
+    {
+        已完成 = 1,
+        待完成 = 2
     }
 }
